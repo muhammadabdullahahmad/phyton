bacb83fd018717695973a54f536a8ab2dec90901
muhammad abdullah <semester1.mitho@gmail.com>
2025-06-20 19:24:34 +0500

    Add files via upload

---
+++ data types.py
@@ -0,0 +1,13 @@
+print("introuduction to data types and opraters")
+a="Abdullah"
+b=15
+c=12.5
+d="true"
+print("name of student is",a)
+print("data type of variable a",type(a))
+print("value in variable b",b)
+print("data type of variable b",type(b))
+print("value in variable c",c)
+print("data type of variable c",type(c))
+print("value in variable d",d)
+print("data type of variable d",type(d))
