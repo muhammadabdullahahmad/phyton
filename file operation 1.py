1dc018dc32e56daa6015366e0ba2d85e6d91c466
muhammad abdullah <semester1.mitho@gmail.com>
2025-08-01 19:19:30 +0500

    Add files via upload

--- file operation 1.py
+++ file operation 1.py
@@ -1,6 +1,21 @@
 file = open ('condingal.txt')
 print (file.read())
 file.close()
+file_read = open ('condingal.txt','r')
+print ("file in read mode -")
+print ("file_read.read()")
+file_read.close
+
+file_write = open ('condingal.txt','w')
+file_write.write("file in write mode")
+file_write.write("hi ! my favourite animal is cat")
+file_write.close
+
+file_append = open ('condingal.txt','a')
+file_append.write("file in append mode")
+file_append.write("hi ! i am cat i am 3 months old")
+file_append.close
+
 
 
 
